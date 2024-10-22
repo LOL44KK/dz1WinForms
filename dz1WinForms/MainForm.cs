21f0aab3e0af57e7b2ddd7134c2e14fdfe837f9b
nikit <nikit@MYKYTATHINKBOOK>
2024-10-22 19:43:24 +0300

    123

--- dz1WinForms/MainForm.cs
+++ dz1WinForms/MainForm.cs
@@ -9,17 +9,28 @@ namespace dz1WinForms
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            Button[,] buttons = new Button[6, 6];
-            for (int i = 0; i < 6; i++)
+            int M = 6;
+            int N = 6;
+
+            Button[,] buttons = new Button[M, N];
+            for (int i = 0; i < M; i++)
             {
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < N; j++)
                 {
                     buttons[i, j] = new Button();
-                    buttons[i, j].Size = new Size(30, 30);
-                    buttons[i, j].Location = new Point(i * 35, j * 35);
+                    buttons[i, j].Size = new Size(60, 60);
+                    buttons[i, j].Location = new Point(i * 65, j * 65);
+                    buttons[i, j].Name = (i + 1).ToString() + "|" +(j + 1).ToString();
+                    buttons[i, j].Click += Button_Click;
+                    buttons[i, j].BackColor = Color.FromKnownColor((KnownColor)new Random().Next(0, 175));
                     Controls.Add(buttons[i, j]);
                 }
             }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(((Button)sender).Name);
         }
     }
 }
