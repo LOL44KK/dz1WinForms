acae6e0d281dd0a94b2a6d044f07caee27629ca4
nikit <nikit@MYKYTATHINKBOOK>
2024-10-22 19:44:13 +0300

    123

--- dz1WinForms/MainForm.cs
+++ dz1WinForms/MainForm.cs
@@ -21,6 +21,7 @@ namespace dz1WinForms
                     buttons[i, j].Size = new Size(60, 60);
                     buttons[i, j].Location = new Point(i * 65, j * 65);
                     buttons[i, j].Name = (i + 1).ToString() + "|" +(j + 1).ToString();
+                    buttons[i, j].Text = (i + 1).ToString() + "|" + (j + 1).ToString();
                     buttons[i, j].Click += Button_Click;
                     buttons[i, j].BackColor = Color.FromKnownColor((KnownColor)new Random().Next(0, 175));
                     Controls.Add(buttons[i, j]);
